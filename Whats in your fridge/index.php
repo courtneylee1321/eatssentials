69baaa56e39ddfd8f22cade757977f3ab0ac649f
sarahlim26 <43418854+sarahlim26@users.noreply.github.com>
2020-12-19 22:00:55 -0500

    Update index.php

--- Whats in your fridge/index.php
+++ Whats in your fridge/index.php
@@ -7,10 +7,10 @@
 
   <!--<div id = "logo"><img src="assets/img/favicon.png" width="55" height="55" ></div>-->
   
-  <title>Whats in your Fridge</title>
+  <title>Whats in Your Fridge</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
-`
+
   <!-- Favicons -->
   <link href="assets/img/favicon.png" rel="icon">
   <link href="assets/img/favicon.png" rel="apple-touch-icon">
@@ -29,7 +29,6 @@
   <!-- Template Main CSS File -->
   <link href="assets/css/style.css" rel="stylesheet">
 
-  <script src="search.js"></script>
   <!-- =======================================================
   * Template Name: Personal - v2.4.0
   * Template URL: https://bootstrapmade.com/personal-free-resume-bootstrap-template/
@@ -44,29 +43,19 @@
   <header id="header" class="header-tops">
     <div class="container">
 
-      <h1><a href="index.php">Whats in your Fridge?</a></h1>
+      <h1><a href="index.html">Whats in your Fridge?</a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="index.html" class="mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a> -->
-      <h2><i>Food Essentials that get you healthy</i></h2>
-      <br>
-      <h2><i>Get started by clicking "Survey"</i></h2>
+      <h2><i>Get started by making a list of foods in your fridge</i></h2>
 
-
-      <form id="f1" name="f1" class="search">
-      	<input type="text" id="t1" name="t1" placeholder="Search recipes here..."
-      	required="">
-      	<input type="button" name="b1" value="SEARCH" form="f1" onclick="getRecipe(document.getElementById('t1').value)">
-      </form>
-
-      <br>
 
       <nav class="nav-menu d-none d-lg-block">
         <ul>
           <li class="active"><a href="#header">Home</a></li>
-          <li><a href="#contact">Survey</a></li>
           <li><a href="#dashboard">Dashboard</a></li>
           <li><a href="#recipes">Recipes</a></li>
-          
+          <li><a href="#survey
+            ">Survey</a></li>
         </ul>
       </nav><!-- .nav-menu -->
 
@@ -77,98 +66,63 @@
     </div>
   </header><!-- End Header -->
 
-  <!-- ======= Dashboard Section ======= -->
+  <!-- ======= About Section ======= -->
   <section id="dashboard" class="dashboard">
 
     <html>
-  <form action="insert.php" method="post">
-  Add Ingredient: <input type="text" name="ingredient" /><br><br>
-  <input type="submit" />
-  </form>
-</html>
+      <form action="insert.php" method="post">
+      Add Ingredient: <input type="text" name="ingredient" /><br><br>
+      <input type="submit" />
+    </form>
+    </html>
 
-<?php
-require_once('connection.php');
+    <?php
+    require_once('connection.php');
 
-      //delete row on button click
-      if(isset($_GET["delete"])){
-                $ID = $_GET["delete"];
-                if($connection->query("DELETE FROM fridge WHERE ID=$ID")){
-                     header('Location: index.php#dashboard');
+          //delete row on button click
+          if(isset($_GET["delete"])){
+                    $ID = $_GET["delete"];
+                    if($connection->query("DELETE FROM fridge WHERE ID=$ID")){
+                         header('Location: ingredients.php');
+                    }  
                 }  
-            }  
 
-        $result = mysqli_query($connection, "SELECT * FROM fridge ORDER BY ID");
+            $result = mysqli_query($connection, "SELECT * FROM fridge ORDER BY ID");
 
-        echo "<table id='fridge'>
-        <thead>
-        <tr>
-        <th>Ingredient</th>
-        <th></th>
-        </tr>
-        </thead>";
-
+            echo "<table id='fridge'>
+            <thead>
+            <tr>
+            <th>Ingredient</th>
+            <th></th>
+            </tr>
+            </thead>";
 
 
-        while($row = mysqli_fetch_array($result))
-        {   
-            echo "<tbody>";
-            echo "<tr>";
-            echo "<td>" . $row['Ingredient'] . "</td>";
-            echo "<td><a class='button alert' href='index.php?delete=".$row["ID"]."'>Delete</a></td>";
-            echo "</tr>";
-        }
-     echo "</tbody>";   
-     echo "</table>";
+
+            while($row = mysqli_fetch_array($result))
+            {   
+                echo "<tbody>";
+                echo "<tr>";
+                echo "<td>" . $row['Ingredient'] . "</td>";
+                echo "<td><a class='button alert' href='ingredients.php?delete=".$row["ID"]."'>Delete</a></td>";
+                echo "</tr>";
+            }
+         echo "</tbody>";   
+         echo "</table>";
 
 
     ?>
    
-
-  <?php
-
-    $sth = mysqli_query($connection, "SELECT Ingredient FROM fridge");
-    $rows = array();
-    while($r = mysqli_fetch_assoc($sth)) {
-        $rows[] = $r['Ingredient'];
-    }
-    $string = json_encode($rows);
-    ?>
-
-
- 
-
   </section><!-- End About Section -->
 
   <!-- ======= Recipes Section ======= -->
   <section id="recipes" class="recipes">
     
-  <form id="f1" name="f1" class="search">
-        <input type="text" name="user" id="mytext">
-        <input type="button" name="b1" value="SEARCH" form="mytext" onclick="getRecipe(document.getElementById('mytext').value)">
-      </form>
-
-   <script type="text/javascript">
-    var rows1 = <?php echo json_encode($rows); ?>;
-
-    var myJSON = JSON.stringify(rows1);
-
-    var parsed = myJSON.replace(/[&\/\\#+()$~%.'":*?<>{}\[\]]/g,'');
-    console.log(parsed);
-
-    var ing = JSON.stringify(parsed);
-    console.log(ing);
-
-    document.getElementById("mytext").value = parsed;
-  </script>
-
-  <div id="results"></div>
-    <div id="results1"></div>
   </section><!-- End Resume Section -->
 
 
   <!-- ======= Contact Section ======= -->
-  <section id="contact" class="contact">
+  <section id="survey" class="survey">
     <div class="container">
 
       <div class="section-title">
@@ -177,17 +131,21 @@ require_once('connection.php');
         Please check all of the health conditions that apply to you: 
       </div>
 
-      <form action="forms/contact.php" method="post" role="form" class="php-email-form mt-4">
-              <input type="checkbox" name="check[]" value="val1" id="diabetes" onclick="checkbox()" unchecked>Diabetes</input>
-              <br>
-              <input type="checkbox" name="check[]" value="val2" id="vitamina" onclick="checkbox()" unchecked>Vitamin A Deficiency</input>
-              <br>
-              <input type="checkbox" name="check[]" value="val3" id="calcium" onclick="checkbox()" unchecked>Calcium Deficiency</input>
-              <br>
-              <input type="checkbox" name="check[]" value="val4" id="iron" onclick="checkbox()" unchecked>Iron Deficiency</input>
-              <br>
-              <input type="checkbox" name="check[]" value="val5" id="cholesterol" onclick="checkbox()" unchecked>High Cholesterol</input>
-              <br>
+
+       <!-- Short survey of a list of health conditions may or may have -->
+       <!-- User will check the boxes that apply to them -->
+       <form method='post' name='template' action="survey.php">
+        <input type="checkbox" id="diabetes" name="survey[]" value="Diabetes">Diabetes</input>
+        <br>
+        <input type="checkbox" id="vitamina" name="survey[]" value="Vitamin A">Vitamin A Deficiency</input>
+        <br>
+        <input type="checkbox" id="calcium" name="survey[]" value="Calcium Deficiency">Calcium Deficiency</input>
+        <br>
+        <input type="checkbox" id="iron" name="survey[]" value="Iron Deficiency">Iron Deficiency</input>
+        <br>
+        <input type="checkbox" id="cholesterot" name="survey[]" value="High Cholesterol">High Cholesterol</input>
+        <br>
+        <input type="submit" value="Submit">
 
       </form>
 
@@ -209,50 +167,7 @@ require_once('connection.php');
 
   <!-- Template Main JS File -->
   <script src="assets/js/main.js"></script>
-
-  <script>
-    // checks which health conditions user checked
-    function checkbox()
-        {
-            var check1 = document.getElementById ("diabetes");
-            var check2 = document.getElementById ("vitamina");
-            var check3 = document.getElementById ("calcium");
-            var check4 = document.getElementById ("iron");
-            var check5 = document.getElementById ("cholesterol");
-
-            if (check5.checked == true)
-            {
-                console.log ("cholesterol")
-                return true;
-            } 
-            if (check4.checked == true)
-            {
-                console.log ("iron")
-                return true;
-            }
-            if (check3.checked == true)
-            {
-                console.log ("calcium")
-                return true;
-            }
-            if (check2.checked == true)
-            {
-                console.log ("vitamina")
-                return true;
-            }
-            if (check1.checked == true)
-            {
-                console.log ("diabetes")
-                return true;
-            } 
-            
-            else
-            {
-                console.log("false")
-                return false;
-            }
-        }
-  </script>
+  
 
 </body>
 
